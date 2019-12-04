04f57a4c705cd768e0630c07f553ff541d4ea914
Yana <33156136+YanaSezko@users.noreply.github.com>
2019-12-04 13:37:09 +0200

    store

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import state, {subscribe} from './redux/State';
+import store from './redux/State';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
-import {addPost, updateNewPostText} from "./redux/State";
+
 
 let rerenderEntireTree = (state)=>{
     ReactDOM.render(
         <BrowserRouter>
-            <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+            <App state={state} addPost={store.addPost.bind(store)} updateNewPostText={store.updateNewPostText.bind(store)}/>
         </BrowserRouter>, document.getElementById('root'));
 }
 
-rerenderEntireTree(state);
+rerenderEntireTree(store.getState());
 
-subscribe(rerenderEntireTree);
+store.subscribe(rerenderEntireTree);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/redux/State.js
+++ src/redux/State.js
@@ -1,52 +1,54 @@
-let rerenderEntireTree=()=>{
-    console.log('State chenged');
-}
+let store={
+    _state:{
+        dialogsPage: {
+            messages: [
+                {id: 1, message: 'Hi'},
+                {id: 2, message: 'How are you?'},
+                {id: 3, message: 'Yo'},
+            ],
+            dialogs: [
+                {id: 1, name: 'Yana'},
+                {id: 2, name: 'Olya'},
+                {id: 3, name: 'Sveta'},
+                {id: 4, name: 'Vasya'},
+                {id: 5, name: 'Sasha'},
 
-
-let state = {
-    dialogsPage: {
-        messages: [
-            {id: 1, message: 'Hi'},
-            {id: 2, message: 'How are you?'},
-            {id: 3, message: 'Yo'},
-        ],
-        dialogs: [
-            {id: 1, name: 'Yana'},
-            {id: 2, name: 'Olya'},
-            {id: 3, name: 'Sveta'},
-            {id: 4, name: 'Vasya'},
-            {id: 5, name: 'Sasha'},
-
-        ]
+            ]
+        },
+        mainPage:{
+            posts: [
+                {id: 1, message: 'это мой первый пост', likesCount: 20},
+                {id: 2, message: 'это мой второй пост', likesCount: 33}
+            ],
+            newPostText:'natural-soap'
+        }
     },
-    mainPage:{
-        posts: [
-            {id: 1, message: 'это мой первый пост', likesCount: 20},
-            {id: 2, message: 'это мой второй пост', likesCount: 33}
-        ],
-        newPostText:'natural-soap'
+    getState(){
+        return this._state;
+    },
+    _callSubscriber() {
+        console.log('State chenged');
+    },
+    addPost(){
+          let newPost = {
+            id:3,
+            message:this._state.mainPage.newPostText,
+            likesCount:0
+        };
+        this._state.mainPage.posts.push(newPost);
+        this._callSubscriber(this._state);
+
+    },
+    updateNewPostText(newText) {
+        this._state.mainPage.newPostText=newText;
+        this._callSubscriber(this._state);
+
+    },
+    subscribe(observer){
+        this._callSubscriber=observer;
     }
-}
-
-export const addPost = (postMess) => {
-       let newPost = {
-        id:3,
-        message:postMess,
-        likesCount:0
-    };
-    state.mainPage.posts.push(newPost);
-    rerenderEntireTree(state);
 
 }
 
-export const updateNewPostText = (newText) => {
-    state.mainPage.newPostText=newText;
-    rerenderEntireTree(state);
-
-}
-
-export const subscribe=(observer)=>{
-rerenderEntireTree=observer;
-}
-
-export default state;
+export default store;
+window.store=store;
