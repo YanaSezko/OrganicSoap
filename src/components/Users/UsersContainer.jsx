6984b085216f2e0b24ec8ca757827ffe4cec9f8c
Yana <33156136+YanaSezko@users.noreply.github.com>
2020-02-14 20:49:14 +0200

    preloader

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -11,6 +11,7 @@ import {
 import * as axios from 'axios';
 import Users from './Users';
 import preloader from '../../images/preloader.svg';
+import Preloader from "../common/Preloader/Preloader";
 
 class UsersContainer extends React.Component {
     componentDidMount() {
@@ -35,7 +36,7 @@ class UsersContainer extends React.Component {
     render() {
 
         return <>
-            {this.props.isFetching ? <img src={preloader}/> : null}
+            {this.props.isFetching ? <Preloader /> : null}
             <Users totalUsersCount={this.props.totalUsersCount}
                    pageSize={this.props.pageSize}
                    currentPage={this.props.currentPage}
