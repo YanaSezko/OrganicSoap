f327e34eaca3340c5a439509c2d05d1711168efa
Yana <33156136+YanaSezko@users.noreply.github.com>
2020-05-22 23:03:03 +0300

    paginator

--- src/components/common/Paginator/Paginator.jsx
+++ src/components/common/Paginator/Paginator.jsx
@@ -2,21 +2,16 @@ import React,{useState} from 'react';
 import styles from "./Paginator.module.css";
 import cn from "classnames";
 
-
 let Paginator = ({totalItemsCount, pageSize, currentPage, onPageChanged, portionSize=10}) => {
-
     let pagesCount = Math.ceil(totalItemsCount / pageSize);
-
     let pages = [];
     for (let i = 1; i <= pagesCount; i++) {
         pages.push(i);
     }
-
     let portionCount =  Math.ceil(pagesCount/portionSize);
     let [portionNumber, setPortionNumber] = useState(1);
     let leftPortionPageNumber = (portionNumber - 1)*portionSize+1;
     let rightPortionPageNumber = portionNumber*portionSize;
-
     return(
         <div className = {styles.paginator}>
             {portionNumber > 1 && 
